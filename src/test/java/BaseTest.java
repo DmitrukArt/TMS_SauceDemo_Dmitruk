ab9616ebfc6687de89eedb0663b5e09e453e1978
DmitrukArt <artem.dmitruk.95@mail.ru>
2021-11-09 16:24:44 +0300

    Initial commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,30 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeClass(alwaysRun = true)
+    public void SetUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @AfterMethod
+    public void deleteAllCookies() {
+        driver.manage().deleteAllCookies();
+    }
+}
